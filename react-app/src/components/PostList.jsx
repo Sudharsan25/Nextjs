115caf123a0b1c65b03664f94021e1b4192c7175
Sudharsan Senthil kumar <sudharsanskumar0125@gmail.com>
2025-01-18 22:08:31 -0500

    Finalized adding and fetching posts from backend

--- react-app/src/components/PostList.jsx
+++ react-app/src/components/PostList.jsx
@@ -1,36 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css';
 
 export default function PostList({ showModal, onCloseModal }) {
-	const [postBody, setPostBody] = useState('');
-	const [postAuthor, setPostAuthor] = useState('');
+	const [posts, setPosts] = useState([]);
+	const [isFetching, setFetching] = useState(false);
 
-	function changeAuthorHandler(event) {
-		setPostAuthor(event.target.value);
-	}
+	// You cannot Fetch data from the API like this directly as it will cause infinite recursion - solution - useEffect() hook
+	// fetch('http://localhost:8080/posts')
+	// 	.then((response) => response.json())
+	// 	.then((data) => setPosts(data.posts));
 
-	function changeBodyHandler(event) {
-		setPostBody(event.target.value);
+	useEffect(() => {
+		async function getPosts() {
+			setFetching(true); // show loading spinner before fetching data
+			const response = await fetch('http://localhost:8080/posts');
+			const data = await response.json();
+			setPosts(data.posts);
+			setFetching(false); // hide loading spinner after fetching data
+		}
+
+		getPosts(); // this will fetch data from the API and set it to the state
+	}, [posts]); // this array has the dependencies of the useEffect - when this array changes, useEffect is executed again
+
+	function addPostHandler(postData) {
+		fetch('http://localhost:8080/posts', {
+			method: 'POST',
+			body: JSON.stringify(postData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		setPosts((existingPosts) => [postData, ...existingPosts]); /// use this method when the old state is dependent on the new state
 	}
 
 	return (
 		<>
 			{showModal ? (
 				<Modal onBackdropClicked={onCloseModal}>
-					<NewPost
-						onBodyChange={changeBodyHandler}
-						onAuthorChange={changeAuthorHandler}
-					/>
+					<NewPost onCancel={onCloseModal} onAddPost={addPostHandler} />
 				</Modal>
 			) : null}
+			{!isFetching && posts.length > 0 && (
+				<ul className={classes.posts}>
+					{posts.map((post) => (
+						<Post key={post.body} author={post.author} body={post.body} />
+					))}
+				</ul>
+			)}
 
-			<ul className={classes.posts}>
-				<Post author={postAuthor} body={postBody} />
-				<Post author="Prachi" body="Hello everyone." />
-			</ul>
+			{!isFetching && posts.length === 0 && (
+				<p style={{ textAlign: 'center', color: 'white' }}>
+					No posts yet. Add a new one by clicking the button above.
+				</p>
+			)}
+			{isFetching && <p>Loading posts!</p>}
 		</>
 	);
 }
