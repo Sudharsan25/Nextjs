1a4c923e5e23295b152250564336ab8897bdeb27
Sudharsan Senthil kumar <sudharsanskumar0125@gmail.com>
2025-01-31 14:24:07 -0500

    Setup Meals page and SQLite DB

--- next-essentials/app/meals/page.js
+++ next-essentials/app/meals/page.js
@@ -1,5 +1,22 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage(){
-    return <main>
-        <h1>This is the meal page</h1>
+    return <>
+    <header className={classes.header}>
+        <h1>A meal page {' '} 
+            <span className={classes.highlight}> for you</span>
+        </h1>
+        <p>Check out cool new reciepes shared by users across the world! </p>
+        <p className={classes.cta}>
+            <Link href='/meals/share'>
+            Share your own reciepes
+            </Link>
+        </p>
+    </header>
+    <main className={classes.main}>
+        <MealsGrid meals={[]} />
     </main>
+    </>
 }
